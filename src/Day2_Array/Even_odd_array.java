8475bf6618552619e5d521c55c3fadcde57141c2
puja kohale <kohalepuja7@gmail.com>
2026-03-09 10:24:02 +0530

    remove colon

--- src/Day2_Array/Even_odd_array.java
+++ src/Day2_Array/Even_odd_array.java
@@ -18,7 +18,7 @@ for(int i=0;i<a.length;i++) {
 System.out.println("Odd number in an array: ");
 for(int i=0;i<a.length;i++) {
 	if(a[i]%2!=0)
-		System.out.println(a[i]);
+		System.out.println(a[i])
 }
 	}
 
